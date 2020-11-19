98b7ba76b31b010b279a90714e20f972437b46ea
DavidJ20 <45216872+DavidJ20@users.noreply.github.com>
2020-11-19 00:31:19 -0500

    Add files via upload

--- Wing Deformation Algorithms/fwing.py
+++ Wing Deformation Algorithms/fwing.py
@@ -5,44 +5,50 @@ Created on Fri Oct  2 02:14:26 2020
 Library used to solve for the emergent wing kinematics in accordance with Moore 2017
 @author: David Yudin
 """
-#%%
+#%% Libraries
 import numpy as np
 from scipy.fft import dct as dct
 from scipy.fft import idct as idct
 from scipy.fft import dst as dst
 from scipy.fft import idst as idst
 import scipy.special
-from scipy.sparse.linalg import gmres, aslinearoperator
-import pdb
-#%%
+from scipy.sparse.linalg import gmres,LinearOperator
+from numpy.polynomial.chebyshev import chebval as chebval
+from scipy.integrate import trapz
+
+#%% fct
 def fct(f):
 # =============================================================================
 #     INPUT: Function evauluated at callocation points
 #     OUTPUT: Chebyshev coefficients  
 #==============================================================================
     ##Calculate chebyshev coefficients with DCT
-    b = dct(f)/(len(f))#*np.sqrt(1/(2*len(f)))
+    b = dct(f)/(len(f))
     ##Correct to coincide with Moore 2017
-    b[0] = b[0]/2#*np.sqrt(2)
-    #b[1:-1] = b[1:-1]*np.sqrt(2/(N))
-
-    
+    b[0] = b[0]/2
     return b
 
-#%%
+
+#%% ifct
 def ifct(f):
 # =============================================================================
 #     INPUT: Chebyshev coefficients
 #     OUTPUT: Inverse discrete cosine tranform of f (f is in spectral space). This function takes f from spectral space to physical space
 #==============================================================================
-    ##Calculate chebyshev coefficients with DCT
-    F = dct(f,type = 3)
-    ##Correct to coincide with Moore 2017
-    F[0] = F[0]#*2#*np.sqrt(2)
-    #b[1:-1] = b[1:-1]*np.sqrt(2/(N))
+    # ##Calculate chebyshev coefficients with DCT
+    N = 1001
+    n = np.arange(0,N,1)
+    theta = np.pi*(2*n+1)/((2*(N)))
+    x = np.cos(theta)
+    #Correct to coincide with Moore 2017
     
+    #f[0] = f[0]*2
+    #F = idct(f)*len(f)
+    F = chebval(x,f)
+    print('i')
     return F
-#%%
+
+#%% fst
 def fst(f):
 # =============================================================================
 #     INPUT: Function evauluated at callocation points
@@ -52,21 +58,22 @@ def fst(f):
     a = dst(f)/len(f)
     ##Correct to coincide with Moore 2017
     a[0] = a[0]/2
-    #b[1:-1] = b[1:-1]*np.sqrt(2/(N))
+    
     return a
-    #%%
+#%% ifst
 def ifst(a):
 # =============================================================================
 #     INPUT: Function evauluated at callocation points
 #     OUTPUT: Chebyshev coefficients  
 #==============================================================================
     ##Calculate chebyshev coefficients with DCT
-    f= idst(a)/len(a)
     ##Correct to coincide with Moore 2017
-    #b[0] = b[0]/2
-    #b[1:-1] = b[1:-1]*np.sqrt(2/(N))
+    a[0] = a[0]*2
+    f= idst(a)*len(a)
+
     return f
-#%%
+
+#%% Theodorsen
 def Theodorsen(sigma):
 # =============================================================================
 #     INPUT: reduced frequency sigma
@@ -74,123 +81,157 @@ def Theodorsen(sigma):
 #==============================================================================
     C_sigma = scipy.special.kv(1,1j*sigma)/(scipy.special.kv(0,1j*sigma)+(scipy.special.kv(1,1j*sigma)))
     return C_sigma
-#%%
+
+#%% cheb_diff
 def cheb_diff(b):
 # =============================================================================
 #     INPUT: N chebyshev coefficients of b
 #     OUTPUT: The derivative of b (bp=b') in spectral space
 #==============================================================================
-	bp = np.zeros((len(b)+1,1))
-	#bp[N] = 0; bp[N-3] = 0
-	for k in np.arange(len(b)-2,-1,-1):
-		bp[k] = bp[k+2] + 2*(k+1)*b[k+1]
+    bp = np.zeros((len(b)+1,1)) + 1j*np.zeros((len(b)+1,1))
+	
+    for k in np.arange(len(b)-2,-1,-1):
+        bp[k] = bp[k+2] + 2*(k+1)*b[k+1]
 		
-        
-	bp[0] = bp[0]/2 
-	bp = np.delete(bp,len(bp)-1,0)
-	return bp
+    bp[0] = bp[0]/2
+    bp = np.delete(bp,len(bp)-1,0)
+    bp = bp.reshape(len(b),)
 
-#%%
-def algorithm1(b):
+    
+    return bp
+
+#%% algorithm1
+def algorithm1(b,x_end):
 # =============================================================================
-#     INPUT: N+1 chebyshev coefficients of g
+#     INPUT: N+1 chebyshev coefficients of b
 #     OUTPUT: The second antiderivative of f (in spectral space)
 #==============================================================================
     N = len(b)
     #Calculate the first antiderivative
-    B = np.zeros((N,1))
-    B[0] = b[0]/4
+    B = np.zeros((N,)) + 1j*np.zeros((N,))
+    #B[0] = b[0]/2
     B[1] = b[0] - b[2]/2
     
     for k in np.arange(2,N-1,1):
         B[k] = (1/(2*k))*(b[k-1]-b[k+1])
-        
-    ##Enforce boundary conditions
-        
-    ##x = 1
-    D_1g = 0
+    
+    #B[0] = b[0]/2
+    ##Enforce boundary conditions f(x_end) = 0
+    
+    D_1g = 0 +0*1j
     for k in np.arange(1,N,1):
-        D_1g = D_1g + (-1)**k * B[k]
+        D_1g = D_1g + (x_end)**k * B[k]
         
-    D_1g = D_1g +B[0]/2
+    D_1g = D_1g + B[0]/2 #sum prime notation
     
 
-    B[0] = B[0] - D_1g #- D_m1g
+    B[0] = B[0] - D_1g 
         
-    f = np.zeros((N,1))
+    f = np.zeros((N,)) + 1j*np.zeros((N,))
 
-    f[0] = B[0]/4
+    #f[0] = B[0]/2
     f[1] = B[0] - B[2]/2
     
     ##Calculate the second antiderivative
-    for k in np.arange(2,N-2,1):
+    for k in np.arange(2,N-1,1):
         f[k] = (1/(2*k))*(B[k-1]-B[k+1])
-        #f[k] = (1/(2*k))*((1/(2*(k+1))*(b[k]-b[k+2])) - (1/(2*(k-1)))*(b[k-2]-b[k]))
-        #f[k] = (b[k]-b[k+2])/(4*k*(k+1)) - (b[k-2]-b[k])/(4*k*(k-1))
         
-    ##Enforce boundary conditions
-        
-    ##x = -1
-    D_m2g = 0
+    #f[0] = B[0]/2
+    ##Enforce boundary condition f(x_end) = 0
+    
+    D_m2g = 0 + 0*1j
     for k in np.arange(1,N,1):
-        D_m2g = D_m2g + (-1)**k * f[k]
-    D_m2g =  D_m2g + f[0]/2
+        D_m2g = D_m2g + (x_end)**k * f[k]
+    
+    D_m2g =  D_m2g + f[0]/2 #sum prime notation
     
     f[0] = f[0] - D_m2g
      
-    print('D_1g:',D_1g)
-    print('D_m2g:',D_m2g)
-    #pdb.set_trace()
+    #f = f.reshape(N,)
 
     return f
-#%%
-def algorithm2(eta_trial,eta_singular,alpha,beta):
+
+#%% algorithm2
+def algorithm2(eta_trial,eta_singular,alpha,beta,U,sigma):
 # =============================================================================
 #     INPUT: N+1 chebyshev coefficients of the trial kinematics (eta_trial) and eta_singular, and alpha and beta
 #     OUTPUT: The operator L applied to the trial kinematics (eta_trial)
 #==============================================================================
-    U = 1 # Nondimensional freestream velocity
-    sigma = 1
-    eta_trial = np.transpose(np.array(fct(eta_trial),ndmin = 2))
+    #1 Evaluate eq(26) for d/dx (Psi)
     aa = 4*np.pi**2 * eta_trial
-    bb = - 4*np.pi*1j*U*cheb_diff(eta_trial)
-    cc = - U**2 * cheb_diff(cheb_diff(eta_trial))
+    bb = (- 4*np.pi*1j*U*cheb_diff(eta_trial)).reshape(len(eta_trial),)
+    cc = (- U**2 * cheb_diff(cheb_diff(eta_trial))).reshape(len(eta_trial),)
     DPsi = aa + bb + cc
-    Psi = np.zeros((len(DPsi),1)) + 1j*np.zeros((len(DPsi),1)) 
+    Psi = np.zeros((len(DPsi),)) + 1j*np.zeros((len(DPsi),))
     
-    Psi[0] = DPsi[0]/4
     Psi[1] = DPsi[0] - DPsi[2]/2
-    
-    #Integrate DPsi once to solve for a_k, k = 1,...,inf. We can't find a_0 yet (constant of integration)
+    #Integrate DPsi once to solve for a_k, k = 1,...,N. We can't find a_0 yet (constant of integration)
     for k in np.arange(2,len(DPsi)-1,1):
         Psi[k] = (1/(2*k))*(DPsi[k-1]-DPsi[k+1])
-        
-    a_k = Psi #get the coefficients a_k
     
-    Q_r = ifst(a_k) #Compute the regular part of the hydrodynamic load with the ifst from a_k's
-    eta_trial = ifct(eta_trial) #transform back to physical space from spectral space
+        
+    
+    
+    a_k = Psi #get the coefficients a_k
+     
+    Q_r = fst(a_k).reshape(len(eta_trial),) #Compute the regular part of the hydrodynamic load with the ifst from a_k's
+    eta_trial = ifct(eta_trial) #4: transform back to physical space from spectral space
     
     beta_eta_Q_r = beta*eta_trial + Q_r
     
-    
-    P_m1_beta_eta_Q_r = algorithm1(fct(beta_eta_Q_r)) #Transform back to spectral space and apply algorithm1
+    P_m1_beta_eta_Q_r = fct(beta_eta_Q_r)
+    P_m1_beta_eta_Q_r = algorithm1(P_m1_beta_eta_Q_r,1)
+    P_m1_beta_eta_Q_r = ifct(P_m1_beta_eta_Q_r)/alpha
+    P_m1_beta_eta_Q_r = fct(P_m1_beta_eta_Q_r)
+    P_m1_beta_eta_Q_r = algorithm1(P_m1_beta_eta_Q_r,-1)
+   # P_m1_beta_eta_Q_r = algorithm1(fct(ifct(algorithm1(fct(beta_eta_Q_r),1))/alpha),-1) #Transform back to spectral space and apply algorithm1 with x_end = 1, then transform to physical space and divide by alpha and then apply algorithm1 again, but with x_end = -1
     
     eta_trial = fct(eta_trial) #transform back to spectral space
-    V = 2*np.pi*1j * eta_trial + U * cheb_diff(eta_trial) #compute V(x) from eq(29)
-    V_hat = fct(V) #transform to spectral space
     
-    
+    V_hat = (2*np.pi*1j * eta_trial + U * cheb_diff(eta_trial)) #compute V(x) from eq(29)
     a_0 = -U*Theodorsen(sigma)*(V_hat[0]+V_hat[1]) + U * V_hat[1]
-    eta_s = np.transpose(np.array(fct(eta_singular),ndmin = 2))
-    L = eta_trial - a_0*eta_s - P_m1_beta_eta_Q_r #build the final operator L
     
-    return L
-#%%
-def algorithm3(S,R,sigma,eta_le,eta_le_p,tol,x,eta_singular,alpha,beta,eta_trial):
     
-    Lambda = eta_le + eta_le_p*(x+1)
+
+    #eta_s = np.transpose(np.array(fct(eta_singular),ndmin = 2))
+    l_eta = eta_trial.reshape(len(eta_trial),) - a_0*eta_singular.reshape(len(eta_singular),) - P_m1_beta_eta_Q_r.reshape(len(eta_trial),) #build the final vector L[eta_trial]
     
-    L = aslinearoperator(algorithm2(eta_trial,eta_singular,alpha,beta))
-    eta = gmres(aslinearoperator(algorithm2(eta_trial,eta_singular,alpha,beta)),Lambda,tol)
+    return l_eta
+
+#%% L_eta_func
+def L_eta_func(eta_trial):
+    
+    N = 1001
+    n = np.arange(0,N,1)
+    theta = np.pi*(2*n+1)/((2*(N)))
+    x = np.cos(theta)	
+    b = 1
+    c = 1
+    rho = 1
+    U_inf = 1#4*np.pi**2
+    
+    R = 1
+    f = np.pi*2
+    E = 20
+    beta = 8*np.pi**2*R
+    S =15#E*b**3/(rho*U_inf**2*c**3)
+    sigma =1.5 # np.pi*c*f/U_inf
+    alpha = S*8*np.pi**2/(3*sigma**2)
+    U = 2*np.pi/sigma
+
+    DDeta_singular = (1/(2*alpha))*((2+x)*np.sqrt(1-x**2)-(1+2*x)*np.arccos(x))
+    eta_singular = algorithm1(fct(DDeta_singular),-1)
+    L_eta = algorithm2(eta_trial,eta_singular,alpha,beta,U,sigma)
+    
+    return L_eta.reshape(N,)
+#%% algorithm3
+def algorithm3(eta_le,eta_le_p,tol,x,eta_trial):
+    
+    Lambda = ((eta_le + eta_le_p*x + eta_le_p*x/x + 1j*0*x).reshape(len(x),))
+    Lambda = fct(Lambda)
+    #capL = aslinearoperator(algorithm2(eta_trial,eta_singular,alpha,beta))
+    L = LinearOperator((1001,1001), matvec = L_eta_func)
+
+    eta = gmres(L,Lambda,tol)
     
     return eta
--- Wing Deformation Algorithms/kinematics_main.py
+++ Wing Deformation Algorithms/kinematics_main.py
@@ -3,45 +3,65 @@
 Created on Sun Nov  1 19:06:59 2020
 
 @author: Dave
-"""
+9"""
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import dct as dct
 from scipy.fftpack import idct as idct
-from numpy.polynomial.chebyshev import chebval as chebval
-from scipy.special import eval_chebyt as chebyt
 from fwing import fct as fct
-from fwing import algorithm1 as algorithm1
-from fwing import ifct as ifct
-from fwing import cheb_diff as cheb_diff
-from fwing import algorithm2 as algorithm2
-from fwing import ifst as ifst
-from fwing import fst as fst
-from fwing import algorithm3 as algorithm3
-import pdb
+from fwing import algorithm1, ifct, cheb_diff, algorithm2, ifst, fst, algorithm3, L_eta_func
+from scipy.sparse.linalg import gmres,LinearOperator
+
+
 
 #%%
 #Define the needed parameters and discrete domain (-1,1)
-N = 101
+N = 1001
 n = np.arange(0,N,1)
 theta = np.pi*(2*n+1)/((2*(N)))
-x = np.cos(theta)	
-
-eta_singular = (2+x)*np.sqrt(1-x**2)-(1+2*x)*np.arccos(x)
-
+x = np.cos(theta)
+alpha = 200
+DDeta_singular = (1/(2*alpha))*((2+x)*np.sqrt(1-x**2)-(1+2*x)*np.arccos(x))
+eta_singular = algorithm1(fct(DDeta_singular),-1)
 eta_trial = x #define a trial function for eta
-b = np.transpose(np.array(fct(eta_trial),ndmin = 2)) #find the chebyshev coefficients of the trial function
-alpha = 1
-beta = 1
-sigma = 1
-S = 1
+b = fct(eta_trial) #find the chebyshev coefficients of the trial function
 eta_le = 1
-eta_le_p = 1
-tol = 0.001
-R = 1
-#%%
-#Now we want to use algorithm2 to evaluate the nonlocal operator script(L)
+eta_le_p = 0
+tol = np.ones((N,1))*1e-12
 
-eta = algorithm3(S, R, sigma, eta_le, eta_le_p, tol, x, eta_singular, alpha, beta, eta_trial)
+#%%
+#Now we want to use algorithm3 to evaluate the nonlocal operator script(L) using GMRES to find eta
+
+Lambda = (eta_le + eta_le_p*x + eta_le_p*x/x)
+Lambda = fct(Lambda)
+L = LinearOperator((N,N), matvec = L_eta_func)
+
+eta = gmres(L,Lambda,tol)
+#%%
+plt.plot(x,ifct(eta[0].imag))
+#plt.ylim(0.9,1.2)
+#Lambda = ((eta_le + eta_le_p*(x+1)).reshape(len(x),))
+#plt.plot(x,Lambda)
+
+#%%
+
+h = ifct(eta[0].real)*np.cos(2*np.pi*t) - (eta[0].imag)*np.sin(np.pi*t)
+
+#%%
+# i = 0
+# for t in np.linspace(0,1,100):
+#     plt.plot(x,ifct(eta[0].real)*np.cos(2*np.pi*t) - ifct(eta[0].imag)*np.sin(2*np.pi*t))
+#     plt.xlim()
+#     plt.ylim(-3,6)
+    
+#     plt.savefig(str(i) +'.png')
+#     plt.close()
+#     i = i + 1
+#     #plt.plot(x,np.exp(2*np.pi*t*1j)*ifct(eta[0]))
+
+#%%
+for t in [0,1/8,2/8,3/8]:
+    plt.plot(x,ifct(eta[0].real)*np.cos(2*np.pi*t) - ifct(eta[0].imag)*np.sin(2*np.pi*t))    
+    
